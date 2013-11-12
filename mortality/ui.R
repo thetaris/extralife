b9e4909c66e063de6a832660817bbd38a0c69572
Andreas Grau <grau@thetaris.com>
2013-11-12 12:58:53 +0100

    Changed ui.R to accomodate demography Changed server.R for loading session data only once.

--- mortality/ui.R
+++ mortality/ui.R
@@ -6,7 +6,15 @@ shinyUI(bootstrapPage(
   # Show a plot of the generated distribution
   mainPanel(
     tabsetPanel(
-      tabPanel("Übersicht der Lebensphasen", showOutput("myChart", "nvd3"))
+      tabPanel("Übersicht der Lebensphasen" 
+               ,showOutput("myChart", "nvd3")
+               )
+      ,tabPanel("Vergleich mit Deutschland"
+               ,sliderInput("year", "Jahr", 
+                             min=2009, max=2060, value=2013, animate=animationOptions(interval=4000, loop=T))
+               ,showOutput("demographyPlot", "nvd3")
+              )
+      
       ,tabPanel("Details", plotOutput("mortalityPlot"))
     )
     
