bfc42d6697c8a7a1d28cfb5a0510b13d5bd998d4
Stefan Dirnstorfer <stefan@thetaris.com>
2013-11-27 13:27:01 +0100

    updated recommendation rules

--- recom/allRecoms.R
+++ recom/allRecoms.R
@@ -11,7 +11,7 @@ recommend <- function(recom_id, target_type, target_id, is_satisfied=FALSE) {
 # Define quick access lists
 types = as.numeric(sapply(json, function(iter) {iter$type_id }))
 has <- function(type) { any(types == type) }
-hasAll <- function(type) { all(sapply(types, function(t) { t %in% type})) }
+hasAll <- function(type) { all(sapply(type, function(t) { t %in% types})) }
 hasAny <- function(type) { any(sapply(types, function(t) { t %in% type})) }
 
 # Recommendation 442: Gueltiges Geburtsdatum
@@ -24,28 +24,29 @@ for (node in json) {
 # Recommendation 628: Ich Person anlegen
 if (!any(types==ELTYPE$Ich)) {
   recommend(628, "TERM", 305)
-} else {
-  # Recommendation 624: Hauptausgaben
-  ref <- c(ELTYPE$Miete, 
-           ELTYPE$Hausgeld, 
-           ELTYPE$Kindertagesbetreuung,
-           ELTYPE$Hypothek)
-  if (!hasAny(ref)) {
-    for (missing in ref)
-      recommend(624, "TERM", missing)
-  }
-  
-  # Recommendation 625: Einkommensart
-  if (!hasAny(ELTYPE$Einkommen._)) {
-    for (missing in ELTYPE$Einkommen._)
-      recommend(625, "TERM", missing)
-  }
-  
-  # Recommendation 626: Familie anlegen
-  if (!hasAny(setdiff(ELTYPE$Meine.Familie._, ELTYPE$Ich))) {
-    for (missing in ELTYPE$Partner._)
-      recommend(626, "TERM", missing)
-    for (missing in ELTYPE$Kinder._)
-      recommend(626, "TERM", missing)
-  }
+  recommend(627, "TERM", 305)
+}
+
+# Recommendation 624: Hauptausgaben
+ref <- c(ELTYPE$Miete, 
+         ELTYPE$Hausgeld, 
+         ELTYPE$Kindertagesbetreuung,
+         ELTYPE$Hypothek)
+if (!hasAny(ref)) {
+  for (missing in ref)
+    recommend(624, "TERM", missing)
+}
+
+# Recommendation 625: Einkommensart
+if (!hasAny(ELTYPE$Einkommen._)) {
+  for (missing in ELTYPE$Einkommen._)
+    recommend(625, "TERM", missing)
+}
+
+# Recommendation 626: Familie anlegen
+if (!hasAny(setdiff(ELTYPE$Meine.Familie._, ELTYPE$Ich))) {
+  for (missing in ELTYPE$Partner._)
+    recommend(626, "TERM", missing)
+  for (missing in ELTYPE$Kinder._)
+    recommend(626, "TERM", missing)
 }
