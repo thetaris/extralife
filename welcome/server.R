2e8a9c46a97cacd642a29695a68e3f7fcc509643
Andreas Grau <grau@thetaris.com>
2013-12-11 15:13:36 +0100

    Another Typo.

--- welcome/server.R
+++ welcome/server.R
@@ -10,11 +10,10 @@ shinyServer(function(input, output, session) {
   
   
   # The following is done on change of input
-
-  
   output$welcomeImg <- renderPlot({
     img <- readPNG("welcome.png")
     plot.new()
+    lim = par() 
     rasterImage(img, lim$usr[1], lim$usr[3], lim$usr[2], lim$usr[4], interpolate=TRUE)              
   }, height=650, width=651 )
   
