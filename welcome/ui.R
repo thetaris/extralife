c93be248ca02badf59e1a7e3ce17c5fd7364f5f0
Andreas Grau <grau@thetaris.com>
2013-12-11 14:46:31 +0100

    Created Welcome Report

--- welcome/ui.R
+++ welcome/ui.R
@@ -3,24 +3,13 @@ library(rCharts)
 
 # Define UI for application that plots random distributions 
 shinyUI(basicPage(
-  
-  # Application title
-  headerPanel("Bericht: Versicherungsanalyse"),
-  
-  # Sidebar with a slider input for number of observations
-    
-    
-     
-  
+      
   # Show a plot of the generated distribution
   mainPanel(
-#    plotOutput("distPlot")
-    div(style="width:800px", 
-        tabsetPanel(
-           tabPanel("Risikoabdeckung", chartOutput("myChart","datatables"))
-           ,tabPanel("Vertragsoptimierung")
-           ,tabPanel("Annahmen")
-           )
+    div(style="width:800px"
+        ,tags$h4("Du hast soeben den ersten Report freigeschaltet!")
+        ,tags$p("Weitere Berichte erreichst Du unter Reports.")
+        ,plotOutput("welcomeImg",  width = "100%")
     )
   )
   
