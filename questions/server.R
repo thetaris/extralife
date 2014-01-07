543a999f195aa908082e1adc5f97cd9016185b1a
Andreas Grau <grau@thetaris.com>
2014-01-07 14:21:16 +0100

    New "question" report with link to Google form

---
+++ questions/server.R
@@ -0,0 +1,22 @@
+library(shiny)
+library(rCharts)
+require(png)
+
+# Define server logic required to generate and plot a random distribution
+shinyServer(function(input, output, session) {
+  
+  # The following is done once per session:
+  
+  
+  
+  # The following is done on change of input
+  output$welcomeImg <- renderPlot({
+    img <- readPNG("welcome.png")
+    plot.new()
+    lim = par() 
+    rasterImage(img, lim$usr[1], lim$usr[3], lim$usr[2], lim$usr[4], interpolate=TRUE)              
+  }, height=650, width=651 )
+  
+  
+  
+})
---
+++ questions/ui.R
@@ -0,0 +1,19 @@
+library(shiny)
+library(rCharts)
+
+# Define UI for application that plots random distributions 
+shinyUI(basicPage(
+      
+  # Show a plot of the generated distribution
+  mainPanel(
+    div(style="width:800px"
+        ,tags$iframe(src="https://docs.google.com/forms/d/1aEGhYU961Mh2yIXux5LkHBxdbKY5sNUsuRzLFi91P88/viewform?embedded=true"
+                     , width="760"
+                     , height="500"
+                     , frameborder="0"
+                     , marginheight="0"
+                     , marginwidth="0")        
+    )
+  )
+  
+))
