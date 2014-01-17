e6bc5ec2b03dc9d27b7442fc6f05e338511ab972
Andreas Grau <grau@thetaris.com>
2014-01-17 15:31:59 +0100

    Maximal 2 digits for cents

--- common/utilEL.R
+++ common/utilEL.R
@@ -1,6 +1,7 @@
 renderEuro <- function(betrag){
   if (is.character(betrag)){
     betrag <- as.numeric(betrag)
-  }  
+  } 
+  betrag<-round(betrag,2)
   sprintf("%s €",format(betrag, digits=10, nsmall=2, decimal.mark=",", big.mark="."))
 }
