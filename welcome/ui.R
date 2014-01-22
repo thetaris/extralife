b30048fda7ff4099be86cd21f84ab9a03476c8e7
Andreas Grau <grau@thetaris.com>
2014-01-22 12:21:06 +0100

    "App" instead of "report"

--- welcome/ui.R
+++ welcome/ui.R
@@ -6,9 +6,9 @@ shinyUI(basicPage(
       
   # Show a plot of the generated distribution
   mainPanel(
-    div(style="width:800px"
-        ,tags$h4("Du hast soeben den ersten Report freigeschaltet!")
-        ,tags$p("Weitere Berichte erreichst Du unter Reports.")
+    div(style="width:760px"
+        ,tags$h4("Du hast soeben Deine erste App freigeschaltet!")
+        ,tags$p("Weitere Apps findest Du hier:")
         ,plotOutput("welcomeImg",  width = "100%")
     )
   )
