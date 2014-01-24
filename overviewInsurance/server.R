8be59b77d07ea7a4161f6d23e077e8edd05de8fe
Andreas Grau <grau@thetaris.com>
2014-01-24 14:22:20 +0100

    Included test for overviewInsuranceApp

--- overviewInsurance/server.R
+++ overviewInsurance/server.R
@@ -28,75 +28,7 @@ shinyServer(function(input, output, session) {
     eval(parse(text=cmdStr))    
   }
     
-  output$overviewTable <- renderUI({
-    renderSchaden <- function(logSum){
-      symbol = tags$img(src="http://thetava.com/Shiny_icons/euro.png", width="25", border="0")
-      space = tags$a(".")
-      
-      res = list();
-      for (iter in 1:logSum){
-        res = list(symbol, res)        
-        if ( ((iter %% 3) == 0) & (iter<logSum) ){
-          res = list(space, res)
-        }
-      }
-      return(tags$div(res))
-    }
-    
-    renderAbdeckung <- function(type){
-    # type is 0 to 5 (best)
-      return(tags$img(src=sprintf("http://thetava.com/Shiny_icons/Signal_%i.png",type), width="25"))
-    }
-    
-    renderStatus <- function(type=0){
-      if (type==0) {
-        return(tags$img(src="http://thetava.com/Shiny_icons/Check_Icon_32.png"))
-      } else {
-        return(tags$img(src="http://thetava.com/Shiny_icons/Warning_Icon_32.png"))
-      }
-    }
-    versicherungen <- getEmpfehlungen(versicherungen, besitz, familie, input)
-        
-    tmp = list(tags$th(tags$h4("Risiko")), tags$th(tags$h4("möglicher Schaden")), tags$th(tags$h4("Abdeckung")), tags$th(tags$h4("Status")))    
-    res = tags$tr(tmp)
-    
-    for (iterRisk in names(versicherungen))
-    {
-      tmp = list(tags$th(div(id= versicherungen[[iterRisk]]$link
-                             ,tags$a( versicherungen[[iterRisk]]$titel)), align="left")
-                ,tags$th(renderSchaden(versicherungen[[iterRisk]]$schaden), align="right")
-                ,tags$th(renderAbdeckung( versicherungen[[iterRisk]]$abdeckung))
-                ,tags$th(renderStatus( versicherungen[[iterRisk]]$status)))    
-      
-      res = list(res, tags$tr(tmp))
-    }              
-    res = tags$table(res, rules="rows", cellpadding="5%", align="center")
-      
-    scripts = list()
-    # iterate over indices of names which is consistent with Tab numbering in JavaScript
-    for (iterRisk in 1:length(names(versicherungen)))
-      {
-        
-      scripts = list(scripts, HTML(sprintf("$('#%s').click(function() {
-                	 tabs = $('.tabbable .nav.nav-tabs li')
-      					 	 tabs.each(function() {
-      							$(this).removeClass('active')
-      					 	 })
-      						 $(tabs[%i]).addClass('active')
-      						
-      						 tabsContents = $('.tabbable .tab-content .tab-pane')
-      					 	 tabsContents.each(function() {
-      							$(this).removeClass('active')
-      					 	 })
-      						 $(tabsContents[%i]).addClass('active')
-      						
-      						$('#%s').trigger('change').trigger('shown');
-      					 })
-        			", versicherungen[[iterRisk]]$link,iterRisk, iterRisk,versicherungen[[iterRisk]]$link)))
-    }
-    res = list(res,tags$script(scripts))
-         
-  }
-  )
+  output$overviewTable <- renderOverview(versicherungen, besitz, familie, input)
+  
   
 })
