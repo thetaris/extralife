13d61aef57362a0ca692be0ce0a163d80b931087
Andreas Grau <grau@thetaris.com>
2014-02-10 22:27:42 +0100

    usse svg grapg device

--- steuertipps/server.R
+++ steuertipps/server.R
@@ -3,7 +3,7 @@ library(rCharts)
 
 require(ggplot2)
 require(gridSVG)
-
+require(XML)
 
 
 # Define server logic required to generate and plot a random distribution
@@ -21,18 +21,24 @@ shinyServer(function(input, output, session) {
   #   ... 
   #    B 17.793359218 19.718587761
   #    B 19.319909163 19.647899863
+  tempsvg <- tempfile(fileext=".svg")
+  svg(tempsvg, width=4, height=4)
   g4 <- ggplot(dat, aes(x=xvar, y=yvar)) +
     geom_smooth() +  #we'll see why order is important
     geom_point(shape=19, aes(color = cond), size=5) 
-  
+  print(g4)
   #export to R object
   #grid.export deprecates the older gridToSVG
-  g4.svg <- grid.export(name = NULL, addClasses=F, progress=T)
+  #g4.svg <- grid.export(name = NULL, addClasses=F, progress=T)
     
   
+  dev.off()
+  svgoutput <- readLines(tempsvg, n=-1)#
+  file.remove(tempsvg)
   # The following is done on change of input
   output$SVGTest <- renderUI({
-    tags$div(HTML(saveXML(g4.svg$svg)))
+    #tags$div(HTML(saveXML(g4.svg$svg)))
+    tags$div(HTML(svgoutput))
   })
   
   
