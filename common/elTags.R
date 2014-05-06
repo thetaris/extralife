1fcb28f1e3f1fd9103029ef618a3cb7475e2ea90
Stefan Dirnstorfer <stefan@thetaris.com>
2014-05-06 11:16:12 +0200

    datasafe tag with relative url

--- common/elTags.R
+++ common/elTags.R
@@ -1,15 +1,15 @@
 elTags <- list(
   img = function(src, ...) {
-    src = paste('https://diegraueseite.de/shiny/extralife/images', src, sep='/')
+    src = paste('/shiny/extralife/images', src, sep='/')
     tags$img(src = src, ...)
   },
   datasafe = function(text, task) {
     list(
       tags$button(class="graypage-open-datasave", "data-entry-task"=task, text),
-      tags$script(type="text/javascript", src="http://nuc-stefan/sites/all/modules/graypagegridster/actions.js")
+      tags$script(type="text/javascript", src="/sites/all/modules/graypagegridster/actions.js")
     )
   },
-  datasafe.float = function(text, task="Daten ändern") {
+  datasafe.float = function(text="Daten ändern", task=0) {
     tags$div(style = 'position:absolute; right:10px; top:10px', elTags$datasafe(text, task))
   }
 )
