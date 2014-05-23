15ba034535314372158078b01499a67f19e982fa
Chao Qi <chao@thetaris.com>
2014-05-23 15:22:36 +0200

    minor change

--- insuranceCheck/ui.R
+++ insuranceCheck/ui.R
@@ -40,8 +40,8 @@ shinyUI(bootstrapPage(
   tabsetPanel(      
         tabPanel(div(class="alloverviewtabs",'ÜBERSICHT'),
                  div(class = "overview", 
-                     tableOutput("main_plot"),          
-                     #TODO: recomtext needs to get from another code
+                     tableOutput("main_plot"),                        
+                     #TODO: recomtext needs to move to the server part and get the content from the recom objetc
                      div(class = "recom",  div(class = "recomPart", div(class ="recomHeadline", "DEINE RISIKOPRÄFERENZ: "), div(class ="recomText", "ICH MÖCHTE MÖGLICHST VIEL ABSICHERN")),                    
                          div(class="changedata", tags$button(class="graypage-open-datasave", "data-entry-task"="insuranceCheck","DATEN ÄNDERN"))
                      )                
@@ -57,4 +57,5 @@ shinyUI(bootstrapPage(
         tabPanel("SCHADEN AM EIGENTUM", detailTab("schadenameigentum", 7)),
         tabPanel("RECHTSSTREIT", detailTab("rechtsstreit", 8))    
   )
+  , addBugMuncher()
 ))
