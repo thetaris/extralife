cb9956a39f8ee8f1b2ee75226b365c50b01061e5
Chao Qi <chao@thetaris.com>
2014-06-02 10:33:10 +0200

    scale

--- peercheck/demodata.R
+++ peercheck/demodata.R
@@ -1,4 +1,6 @@
 n <- 1000
+scale <- runif(n,0,1)
 data <- data.frame(A = floor(runif(n,1,4)),
-                   B = floor(runif(n,1,3)),
-                   C = floor(runif(n,1,5)))
+                   B = floor(runif(n,1,3)*scale),
+                   C = floor(runif(n,1,5)*scale))
+
