88f5b50ae8cbbebadd8408f9d84ad12f97d492d3
Chao Qi <chao@thetaris.com>
2014-06-13 16:54:59 +0200

    add all questions page

--- peercheck/run.R
+++ peercheck/run.R
@@ -7,14 +7,23 @@ source('access.R', encoding='UTF-8')
 source('format.R', encoding='UTF-8')
 
 server <- Rhttpd$new()
-server$add(name="peercheck",
-      app=Builder$new(
-        Brewery$new(url="/", root="www", scheme='https'),
-        Redirect$new("/welcome.html")
-      )
-)
-server$add(app = File$new("style"), name = "style")
-server$start(quiet=TRUE, port=9010)
 
+start <- function(){
+  server$add(name="peercheck",
+             app=Builder$new(
+               Brewery$new(url="/", root="www", scheme='http'),
+               Redirect$new("/welcome.html")
+             )
+  )
+  server$add(app = File$new("style"), name = "style")
+  server$start(quiet=TRUE, port=9010)
+}
 
+start()
 server$browse('peercheck')
+
+while (T) {
+  Sys.sleep(5);
+  start()
+}
+
