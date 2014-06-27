78d87a63cf590864503a2c72d8b5c219a3e1f7b5
Chao Qi <chao@thetaris.com>
2014-06-27 15:11:55 +0200

    bug fix

--- peercheck/run.R
+++ peercheck/run.R
@@ -10,17 +10,31 @@ source('format.R', encoding='UTF-8')
 
 server <- Rhttpd$new()
 
-deploy <- function(){
-  server$add(name="peercheck",
-             app=Builder$new(
-               Brewery$new(url="/", root="www"),
-               Redirect$new("/welcome.html")
-             )
-  )
-  server$add(app = File$new("style"), name = "style")
-}
+server$add(name="peercheck",
+           app=Builder$new(
+             Brewery$new(url="/", root="www"),
+             Redirect$new("/welcome.html")
+           )
+)
+server$add(app = File$new("style"), name = "style")
+server$add(app = File$new("plot"), name = "plot")
 
-deploy()
+server$add(name="plots", function(env) {
+  request <- Request$new(env)
+  response <- Response$new()
+  
+  path <<- request$path()
+  questionid <- unlist(strsplit(path, "/"))[4]
+
+  content <- getSatisfactionHTML(questionid, NULL)
+  content <- paste0('<head><meta content="text/html" charset="UTF-8"></head>', content)
+  
+  response$write(content)
+  #response$write(paste("<img src =", server$full_url("plot"), "/example.png", ">", sep = ""))
+  
+  response$finish()
+})
 server$start(quiet=TRUE, port=9010)
 server$browse('peercheck')
+
 
